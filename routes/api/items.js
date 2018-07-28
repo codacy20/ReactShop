cf07c1edbc4d5b5f4d37a68d0c037d3e7225504c
amir <kmuc72@gmail.com>
2018-07-28 18:27:25 +1000

    post and delete api item

--- routes/api/items.js
+++ routes/api/items.js
@@ -7,11 +7,32 @@ const Item = require('../../models/item');
 // @route GET api/items
 // @desc Get all Items
 // @access public
-
 router.get('/', (req, res) => {
     Item.find()
         .sort({ date: -1 })
         .then(items => res.json(items))
+});
+
+// @route POST api/items
+// @desc post an Item
+// @access public
+router.post('/', (req, res) => {
+
+    const newItem = new Item({
+        name: req.body.name
+    });
+    newItem.save().then(item => res.json(item));
+});
+
+
+// @route Delete api/items
+// @desc delete an Item
+// @access public
+router.delete('/:id', (req, res) => {
+
+    Item.findById(req.params.id)
+        .then(item => item.remove().then((() => res.json({ success: true }))))
+        .catch(err => res.status(404).json({ success: false }))
 });
 
 
